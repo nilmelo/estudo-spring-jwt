c5b00d56c6db3b51b14c5d1175b37e2b596a59df
Nilson Melo <nilson.melo@sti.ufc.br>
2023-09-25 16:20:26 -0300

    feature: Token Generator

--- src/main/java/br/com/treinaweb/javajobs/services/AuthenticationService.java
+++ src/main/java/br/com/treinaweb/javajobs/services/AuthenticationService.java
@@ -1,12 +1,19 @@
 package br.com.treinaweb.javajobs.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.treinaweb.javajobs.auth.AuthenticatedUser;
+import br.com.treinaweb.javajobs.dto.JwtResponse;
+import br.com.treinaweb.javajobs.dto.UserDTO;
 import br.com.treinaweb.javajobs.models.User;
 import br.com.treinaweb.javajobs.repositories.UserRepository;
 
@@ -16,6 +23,12 @@ public class AuthenticationService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User foundUser = userRepository.findByUsername(username)
@@ -23,6 +36,19 @@ public class AuthenticationService implements UserDetailsService {
                 String.format("User with username %s not found", username)));
         
         return new AuthenticatedUser(foundUser);
+    }
+
+    public JwtResponse createJwtResponse(UserDTO userDTO) {
+        String username = userDTO.getUsername();
+        String password = userDTO.getPassword();
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, password);
+        Authentication authenticatedUser = authenticationManager.authenticate(authentication);
+
+        String token = jwtService.generateToken(authenticatedUser);
+        Date expiresAt = jwtService.getExpirationFromToken(token);
+
+        return new JwtResponse(token, "Bearer", expiresAt);
     }
     
 }
