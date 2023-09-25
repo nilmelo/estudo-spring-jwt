08a194a20fb5d242763a1a5bafa2edf1c102eca8
Nilson Melo <nilson.melo@sti.ufc.br>
2023-09-25 13:22:30 -0300

    first commit

---
+++ src/main/java/br/com/treinaweb/javajobs/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package br.com.treinaweb.javajobs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String API_JOBS_URL = "/api/v1/jobs/**";
+    private static final String API_USERS_URL = "/api/v1/users/**";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, API_JOBS_URL).permitAll()
+            .antMatchers(API_USERS_URL).permitAll()
+            .anyRequest().authenticated();
+
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
